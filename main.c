57f69a8d5539bc4ce2f8d90494248da3efaacd61
Selim Efe <uysalselimefe@gmail.com>
2026-01-18 23:09:15 +0300

    Update main.c

--- main.c
+++ main.c
@@ -3,35 +3,35 @@
 
 int main()
 {
-    int sayi,i,kalan,max,b1,b2,b3,b4;
+    int sayi, i, kalan, max, b1, b2, b3, b4;
 
-    sayi=10000;
-    max=sayi;
+    sayi = 10000;
+    max = sayi;
 
-    for(;max>0;max--){
+    for (; max > 0; max--)
+    {
 
-        sayi=max;
+        sayi = max;
 
-        b1=max/1000;
-        kalan=max%1000;
+        b1 = max / 1000;
+        kalan = max % 1000;
 
-        b2=kalan/100;
-        kalan=kalan%100;
+        b2 = kalan / 100;
+        kalan = kalan % 100;
 
-        b3=kalan/10;
-        kalan=kalan%10;
+        b3 = kalan / 10;
+        kalan = kalan % 10;
 
-        b4=kalan;
+        b4 = kalan;
 
-        if(b1!=b2 && b1!=b3 && b1!=b4 &&
-           b2!=b3 && b2!=b4 &&
-           b3!=b4){
-            printf("sonuc: %d",sayi);
+        if (b1 != b2 && b1 != b3 && b1 != b4 &&
+            b2 != b3 && b2 != b4 &&
+            b3 != b4)
+        {
+            printf("sonuc: %d", sayi);
             break;
-           }
-
+        }
     }
-
 
     return 0;
 }
